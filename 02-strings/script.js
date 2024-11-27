430f1eb69c8c988300859538973822291dbfc5f1
Denny Rubino <rubidenny@gmail.com>
2024-11-27 13:25:22 +0100

    fix ex 1-2

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -1,11 +1,19 @@
 // 1. Dichiarazione di variabili
 // Crea una variabile chiamata name e assegnale il tuo nome.
 // Stampa il valore di nome nella console.
-
+const name= 'Denny';
+console.log(name);
 // 2. Tipi primitivi
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
+const number= '13';
+const text= 'Ciao';
+const bool= '0';
+console.log(number);
+console.log(text);
+console.log(bool);
+
 
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
